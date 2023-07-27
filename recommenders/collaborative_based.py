872cce799410b181b6315f5dbe287e0b59fe5c5a
Mati Matlakala <mati.matlakala@gmail.com>
2023-07-27 22:12:35 +0200

    Update collaborative_based.py

--- recommenders/collaborative_based.py
+++ recommenders/collaborative_based.py
@@ -36,6 +36,7 @@ from surprise import Reader, Dataset
 from surprise import SVD, NormalPredictor, BaselineOnly, KNNBasic, NMF
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
+from surprise.model_selection import train_test_split
 
 # Importing data
 movies_df = pd.read_csv('resources/data/movies.csv',sep = ',')
@@ -120,29 +121,27 @@ def collab_model(movie_list,top_n=10):
 
     indices = pd.Series(movies_df['title'])
     movie_ids = pred_movies(movie_list)
-    df_init_users = ratings_df[ratings_df['userId']==movie_ids[0]]
-    for i in movie_ids :
-        df_init_users=df_init_users.append(ratings_df[ratings_df['userId']==i])
+    df_init_users = ratings_df[ratings_df['userId'].isin(movie_ids)]
+
     # Getting the cosine similarity matrix
-    cosine_sim = cosine_similarity(np.array(df_init_users), np.array(df_init_users))
-    idx_1 = indices[indices == movie_list[0]].index[0]
-    idx_2 = indices[indices == movie_list[1]].index[0]
-    idx_3 = indices[indices == movie_list[2]].index[0]
-    # Creating a Series with the similarity scores in descending order
-    rank_1 = cosine_sim[idx_1]
-    rank_2 = cosine_sim[idx_2]
-    rank_3 = cosine_sim[idx_3]
-    # Calculating the scores
-    score_series_1 = pd.Series(rank_1).sort_values(ascending = False)
-    score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
-    score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
-     # Appending the names of movies
-    listings = score_series_1.append(score_series_1).append(score_series_3).sort_values(ascending = False)
+    cosine_sim = pd.DataFrame(cosine_similarity(df_init_users, df_init_users), index=df_init_users.index, columns=df_init_users.index)
+
+    # Use the provided movie_list to find the corresponding movie indices in the cosine_sim DataFrame
+    movie_indices = [indices[indices == movie].index[0] for movie in movie_list]
+
+    # Sum the cosine similarity for each user across the selected movies
+    user_scores = cosine_sim[movie_indices].sum(axis=1)
+
+    # Sort the users based on their similarity scores and get the top N users
+    top_users = user_scores.sort_values(ascending=False)[:top_n]
+
+    # Find the movies rated by the top users and recommend those movies
     recommended_movies = []
-    # Choose top 50
-    top_50_indexes = list(listings.iloc[1:50].index)
-    # Removing chosen movies
-    top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
-    for i in top_indexes[:top_n]:
-        recommended_movies.append(list(movies_df['title'])[i])
-    return recommended_movies
+    for user_id in top_users.index:
+        user_movies = ratings_df[ratings_df['userId'] == user_id]
+        user_movies = user_movies.sort_values(by='rating', ascending=False)
+        recommended_movies.extend(list(user_movies['title'].head(top_n)))
+
+    # Filter out duplicates and return the top N recommended movies
+    recommended_movies = list(set(recommended_movies))
+    return recommended_movies[:top_n]
